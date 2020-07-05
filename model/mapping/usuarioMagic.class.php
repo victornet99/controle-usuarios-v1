284452d8284141d1012ea8b5724c1f4ce604c858
André Lucas Freitas <projetodigitalam@outlook.com>
2020-07-05 16:55:39 -0400

    Adicionado método de mapeamento do banco de dados usando métodos mágicos

---
+++ model/mapping/usuarioMagic.class.php
@@ -0,0 +1,29 @@
+<?php
+
+  /*
+     Esta classe é a mesma declarada anteriormente, apenas com um novo nome e
+     em vez de utilizar os getters e setters vistos anteriormente, utiliza
+     métodos mágicos de acesso aos dados privados
+  */
+
+  class Usuario{
+
+    private $idusuario;
+    private $nomeusuario;
+    private $sobrenome;
+    private $idadeusuario;
+    private $contato;
+    private $login;
+    private $senha;
+
+    # Declaração de métodos mágicos
+
+    public function __get($name){
+      return $this->$name;
+    }
+
+    public function __set($name, $value){
+      $this->$name = $value;
+    }
+
+  }
