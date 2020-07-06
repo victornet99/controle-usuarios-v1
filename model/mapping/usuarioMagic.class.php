5d2563b546453920dcb65405c973056b707a9b91
André Lucas Freitas <projetodigitalam@outlook.com>
2020-07-05 21:26:10 -0400

    Adicionado métodos mágicos __get e __set no projeto

--- model/mapping/usuarioMagic.class.php
+++ model/mapping/usuarioMagic.class.php
@@ -1,21 +1,29 @@
 <?php
 
   /*
-     Esta classe é a mesma declarada anteriormente, apenas com um novo nome e
+     Esta classe é a mesma declarada, apenas com um novo nome e
      em vez de utilizar os getters e setters vistos anteriormente, utiliza
      métodos mágicos de acesso aos dados privados
   */
 
-  class Usuario{
+class Usuario{
 
-    private $idusuario;
-    private $nomeusuario;
-    private $sobrenome;
-    private $idadeusuario;
-    private $contato;
-    private $login;
-    private $senha;
+  private $idusuario;
+  private $nomeusuario;
+  private $sobrenome;
+  private $idadeusuario;
+  private $contato;
+  private $login;
+  private $senha;
 
-    
-
+  # O get retorna o valor contido na variável privada
+  public function __get($name){
+    return $this->$name;
   }
+
+  # O set atribui um valor para dentro da variável privada
+  public function __set($name, $value){
+    $this->$name = $value;
+  }
+
+}
