273123fd6a8bee9165070a3374285eaa1ff31d93
André Lucas Freitas <projetodigitalam@outlook.com>
2020-07-18 20:23:04 -0400

    Alterado arquivo SQL com base de dados usada nos exemplos

--- sql/script-controle-usuarios.sql
+++ sql/script-controle-usuarios.sql
@@ -1,17 +1,63 @@
-create database `controle-usuarios`;
-use `controle-usuarios`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: controle-usuarios
+-- ------------------------------------------------------
+-- Server version	8.0.20
 
-create table `usuario` (
-	idusuario int not null auto_increment primary key,
-    nomeusuario varchar(60) not null,
-    sobrenome varchar(60) not null,
-    contatousuario varchar(60) not null,
-    login varchar(30) not null,
-    senha varchar(30) not null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-alter table `usuario` change column `contatousuario` `contato`
-int not null;
+--
+-- Table structure for table `usuario`
+--
 
-alter table `usuario` add column idadeusuario int not null
-after `sobrenome`;
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `idusuario` int NOT NULL AUTO_INCREMENT,
+  `nomeusuario` varchar(60) NOT NULL,
+  `sobrenome` varchar(60) NOT NULL,
+  `idadeusuario` int NOT NULL,
+  `contato` int NOT NULL,
+  `login` varchar(30) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'controle-usuarios'
+--
+
+--
+-- Dumping routines for database 'controle-usuarios'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-18 20:22:37
