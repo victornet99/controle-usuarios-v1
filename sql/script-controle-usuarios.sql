f4c1b2eaa0763cebfcdf8070a0828e790e012e64
André Lucas Freitas <projetodigitalam@outlook.com>
2020-08-24 13:32:44 -0400

    Alterado script SQL do aplicativo e informações adicionais do PHPStorm

--- sql/script-controle-usuarios.sql
+++ sql/script-controle-usuarios.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `usuario`
 --
 
+CREATE DATABASE IF NOT EXISTS `controle-usuarios`;
+USE `controle-usuarios`;
+
 DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
